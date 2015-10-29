8e9c673241035d865dcecc9b55e0e6435074e5e8
Diego Zamboni <diego.zamboni@swisscom.com>
2015-10-29 01:28:12 +0100

    Added config options for automatic and manual reloading

--- plugins/apps/hammerspoon_config_reload.lua
+++ plugins/apps/hammerspoon_config_reload.lua
@@ -1,22 +1,33 @@
 ---- Configuration file management
+local mod={}
 
--- Manual config reload
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", hs.reload)
+mod.config = {
+   ["auto_reload"]=true,
+   ["manual_reload_key"]={{"cmd", "alt", "ctrl"}, "R"}
+}
 
 -- Automatic config reload if any files in ~/.hammerspoon change
 function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
+   doReload = false
+   for _,file in pairs(files) do
+      if file:sub(-4) == ".lua" then
+         doReload = true
+      end
+   end
+   if doReload then
+      hs.reload()
+   end
 end
 
-hs.pathwatcher.new(hs_config_dir, reloadConfig):start()
+function mod.init()
+   if mod.config.auto_reload then
+      hs.pathwatcher.new(hs_config_dir, reloadConfig):start()
+   end
 
----- Notify when the configuration is loaded
-notify("Hammerspoon", "Config loaded")
+   -- Manual config reload
+   if type(mod.config.manual_reload_key) == "table" then
+      hs.hotkey.bind(mod.config.manual_reload_key[1], mod.config.manual_reload_key[2], hs.reload)
+   end
+end
+
+return mod
