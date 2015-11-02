3c232f665dd74bc251f5cdb5f8bd04f003f57145
Diego Zamboni <diego.zamboni@swisscom.com>
2015-11-02 10:28:28 +0100

    Added more sample local configs

--- init-local-sample.lua
+++ init-local-sample.lua
@@ -1,13 +1,28 @@
+-- Uncomment to set non-default log level
+-- logger.setLogLevel('debug')
+
 -- Local code and config
 
--- Uncomment to set non-default log level
---logger.setLogLevel('debug')
-
---[[Sample plugin configuration
+--[[ Sample configurations
 
 omh_config("mouse.locator",
            {
               ["color"] = hs.drawing.color.rebeccapurple,
+           })
+
+omh_config("apps.universal_archive",
+           {
+              ["evernote_archive_notebook"] = ".Archive",
+              ["evernote_other_archives"] = {
+                 -- Notebook = Key binding
+                 ["SuperProject"] = { {"Ctrl", "Cmd"}, "q" }
+              },
+              ["archive_notifications"] = false,
+           })
+
+omh_config("misc.clipboard",
+           {
+              ["show_menu_counter"] = false,
            })
 
 --]]
