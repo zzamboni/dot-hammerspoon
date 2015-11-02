070d5a9db006801f539f137851975aeed606532f
Diego Zamboni <diego.zamboni@swisscom.com>
2015-11-03 00:21:42 +0100

    Simplified table syntax

--- init-local-sample.lua
+++ init-local-sample.lua
@@ -3,26 +3,31 @@
 
 -- Local code and config
 
---[[ Sample configurations
+--[[
 
 omh_config("mouse.locator",
            {
-              ["color"] = hs.drawing.color.rebeccapurple,
+              color = hs.drawing.color.x11.rebeccapurple,
            })
 
 omh_config("apps.universal_archive",
            {
-              ["evernote_archive_notebook"] = ".Archive",
-              ["evernote_other_archives"] = {
+              evernote_archive_notebook = ".Archive",
+              evernote_other_archives = {
                  -- Notebook = Key binding
-                 ["SuperProject"] = { {"Ctrl", "Cmd"}, "q" }
+                 Quenah = { {"Ctrl", "Cmd"}, "q" }
               },
-              ["archive_notifications"] = false,
+              archive_notifications = false,
            })
 
 omh_config("misc.clipboard",
            {
-              ["show_menu_counter"] = false,
+              show_menu_counter = false,
+           })
+
+omh_config("misc.colorpicker",
+           {
+              colorpicker_in_menubar = false,
            })
 
 --]]
