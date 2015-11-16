77c6773fd9aa29c6e952f50633f03cace7adb0fb
Diego Zamboni <diego.zamboni@swisscom.com>
2015-11-16 14:16:32 +0100

    Moved Hammerspoon-related plugins to the top for clarity

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 require("oh-my-hammerspoon")
 
 OMH_PLUGINS = {
+   "apps.hammerspoon_toggle_console",
+   "apps.hammerspoon_install_cli",
+   "apps.hammerspoon_config_reload",
    "windows.manipulation",
    "windows.grid",
    "apps.skype_mute",
@@ -9,9 +12,6 @@ OMH_PLUGINS = {
    "misc.clipboard",
    "misc.colorpicker",
    "keyboard.menubar_indicator",
-   "apps.hammerspoon_toggle_console",
-   "apps.hammerspoon_install_cli",
-   "apps.hammerspoon_config_reload",
    "apps.universal_archive",
    "windows.screen_rotate"
 }
